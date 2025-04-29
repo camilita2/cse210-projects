5e863e20829a76c2a444f5d165aed7ccec4ed0c5
camilita2 <ram22042@byui.edu>
2025-04-29 10:12:07 -0600

    added for loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        int x = 10;
+if (x == 10) // single = is assignment, == is comparison
+{
+	Console.WriteLine("X is 10");
+}
+
+for (int i = 0; i < x; i++) //i ++ will be executed at the end of the code, i = 0 is where to start
+{
+	Console.WriteLine($"Bob is cool: {i}");
+}
+
     }
 }
+
